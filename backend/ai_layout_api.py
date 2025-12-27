0ba566ae54c0a5b3bf727c564cc14c8963e66a61
Devesh Singh <devesh.random18@gmail.com>
2025-12-27 18:22:57 +0530

    new

--- backend/ai_layout_api.py
+++ backend/ai_layout_api.py
@@ -19,7 +19,7 @@ from PIL import Image, ImageDraw, ImageFont
 # SETUP
 
 load_dotenv()
-GROQ_API_KEY = "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"
+GROQ_API_KEY = "your groq api key"
 
 
 if GROQ_API_KEY:
